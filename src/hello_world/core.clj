a58a7ae9f051cda79528e7b1ee7859e076343188
Ljingx <ljx20132200022@gmail.com>
2021-06-27 13:21:47 +0800

    * init: typos

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,6 +1,11 @@
 (ns hello-world.core)
 
-(defn handler [request]
+(defn handler []
   {:status 200
    :headers {"Content-Type" "Text/html"}
    :body "Hello, word"})
+
+(def a {:a "a" :b "b" :c {:d 2}})
+(println a)
+(def c (assoc-in a [:c :d] 3))
+(println c)
